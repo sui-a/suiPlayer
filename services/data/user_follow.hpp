ccd3312e50373e5f36f82937ceb53d426fdd1005
sui <2734942198@qq.com>
2026-07-02 09:28:47 +0800

    添加用户操作数据结构，用户身份角色操作类，用户角色权限类，以及对应测试，去除多余的build

---
+++ services/data/user_follow.hpp
@@ -0,0 +1,35 @@
+#include <suiScaffold/suiodb.hpp>
+#include <suiScaffold/suiRedis.hpp>
+#include <suiScaffold/suiQueue.hpp>
+#include <suiScaffold/log.h>
+#include "removeCache.hpp"
+#include "data.hpp"
+#include "data-odb.hxx"
+
+namespace suiUserFollow
+{
+    class suiFollow
+    {
+    public:
+        using ptr = std::shared_ptr<suiFollow>;
+
+        suiFollow(odb::database& db);
+
+        //增
+        void insert(const std::string& user_id, const std::string& follow_user_id);
+
+        //删
+        void remove(const std::string& user_id, const std::string& follow_user_id);
+
+        //查
+        bool judgment(const std::string& user_id, const std::string& follow_user_id);
+
+
+    private:
+        //数据库操作句柄
+        odb::database& _db;
+    };
+
+
+
+}
--- services/file/build/CMakeFiles/suiFileService.dir/home/sui/suiPlayer/services/data/user_Identity_role.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/suiFileService.dir/home/sui/suiPlayer/services/data/user_Identity_role.cpp.o: \
- /home/sui/suiPlayer/services/data/user_Identity_role.cpp \
- /usr/include/stdc-predef.h
